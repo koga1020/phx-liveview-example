2354ab94ac469d0272d6fe0bb71ea16fdce204c3
koga1020 <koga.shozo1020@gmail.com>
2019-04-11 00:15:38 +0900

    update install_bulma.sh

--- install_bulma.sh
+++ install_bulma.sh
@@ -3,4 +3,4 @@ sed -i -e "s/'css-loader'/'css-loader', 'sass-loader'/" assets/webpack.config.js
 sed -i -e "s/\\.css/\\.scss/" assets/webpack.config.js 
 sed -i -e "s/app.css/app.scss/" assets/js/app.js 
 mv assets/css/app.css assets/css/app.scss
-echo "@import "./bulma.sass";" >> assets/css/app.scss
+echo "@import \"~bulma/bulma\";" >> assets/css/app.scss
