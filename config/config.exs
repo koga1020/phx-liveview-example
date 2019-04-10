edc32bca099d4382c39cc0fc03b58b91416aec51
koga1020 <koga.shozo1020@gmail.com>
2019-04-10 22:45:11 +0900

    add Endpoint config

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,8 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
+
+config :sample_app, SampleAppWeb.Endpoint,
+   live_view: [
+     signing_salt: "SECRET_SALT"
+   ]
