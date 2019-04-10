82b634f3a55dd413f030fc98ff941e0c8a381463
koga1020 <koga.shozo1020@gmail.com>
2019-04-10 23:00:41 +0900

    add liveview sample file

---
+++ lib/sample_app_web/live/sample_live.ex
@@ -0,0 +1,13 @@
+defmodule SampleAppWeb.SampleLive do
+  use Phoenix.LiveView
+
+  def render(assigns) do
+    ~L"""
+    """
+  end
+
+  def mount(_session, socket) do
+    {:ok, socket}
+  end
+
+end
